e3935f2664e1f67e256da32bd21b130220bc0e95
Mercy Ma <mercyblitz@gmail.com>
2025-02-14 16:20:32 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,10 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.enabled = true
+        html.enabled = true
+    }
 }
 
 tasks.withType<JavaCompile> {
